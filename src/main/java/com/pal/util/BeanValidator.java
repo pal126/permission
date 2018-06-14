ecf5f1bd92b8396e4b819da80bc3aff3bf9b0b93
pal126 <wangkang@jskerun.com.cn>
2018-06-14 09:33:53 +0800

    db

--- src/main/java/com/pal/util/BeanValidator.java
+++ src/main/java/com/pal/util/BeanValidator.java
@@ -1,9 +1,12 @@
 package com.pal.util;
 
+import com.google.common.collect.Maps;
+
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import java.util.Map;
+import java.util.*;
 
 public class BeanValidator {
 
@@ -11,6 +14,19 @@ public class BeanValidator {
 
     public static <T> Map<String, String> validate(T t, Class... groups) {
         Validator validator = validatorFactory.getValidator();
-        return null;
+        Set validatorResult = validator.validate(t, groups);
+        if (validatorResult.isEmpty()) {
+            return Collections.emptyMap();
+        } else {
+            LinkedHashMap errors = Maps.newLinkedHashMap();
+            Iterator iterator = validatorResult.iterator();
+            while (iterator.hasNext()) {
+                ConstraintViolation violation = (ConstraintViolation)iterator.next();
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage());
+            }
+            return errors;
+        }
     }
+
+
 }
